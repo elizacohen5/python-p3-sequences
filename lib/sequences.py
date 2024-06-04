4ab352449837efe0d6ea0dfba6601a6f87524b1a
Eliza Cohen <elizacohen5@gmail.com>
2024-06-04 08:55:31 -0400

    complete sequences lesson

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
 
+#Write a function print_fibonacci() that prints a list of the fibonacci sequence 
+#up to the length provided in the function's parameters.
+
 def print_fibonacci(length):
-    pass
+    fibonacci_list = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+    print(fibonacci_list[0:length])
+
+print_fibonacci(4)
+    
